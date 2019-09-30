4aff2b72d1b18296bb7d5e22e210ff08f22652a6
michalbaranowski <michal_baranowski@o2.pl>
2019-09-30 19:53:28 +0200

    added random delay (1-2s) for every request

--- vod.Domain.Services/Utils/HtmlSource/HtmlSourceGetter.cs
+++ vod.Domain.Services/Utils/HtmlSource/HtmlSourceGetter.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 
@@ -7,10 +9,12 @@ namespace vod.Domain.Services.Utils.HtmlSource
     public class HtmlSourceGetter : IHtmlSourceGetter
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly Random _random;
 
         public HtmlSourceGetter(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
+            _random = new Random();
         }
 
         public async Task<HtmlDocument> GetHtmlFrom(string url)
@@ -18,6 +22,8 @@ namespace vod.Domain.Services.Utils.HtmlSource
             var request = new HttpRequestMessage(HttpMethod.Get, url);
             var client = _httpClientFactory.CreateClient();
             client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+            
+            Thread.Sleep(_random.Next(1000,2000));
 
             var response = await client.SendAsync(request);
             var htmlString = await response.Content.ReadAsStringAsync();
