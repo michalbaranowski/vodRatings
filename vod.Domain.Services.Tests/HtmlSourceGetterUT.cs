609dfcca0c456f8fc865d41d397981f92d6c92c1
michalbaranowski <michal_baranowski@o2.pl>
2019-10-06 14:12:58 +0200

    added unit test for html source getter

---
+++ vod.Domain.Services.Tests/HtmlSourceGetterUT.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Moq;
+using NUnit.Framework;
+using RichardSzalay.MockHttp;
+using vod.Domain.Services.Boundary.Interfaces.Enums;
+using vod.Domain.Services.Utils;
+using vod.Domain.Services.Utils.HtmlSource;
+using static vod.Domain.Services.Utils.NcPlusUrls;
+
+namespace vod.Domain.Services.Tests
+{
+    public class HtmlSourceGetterUT
+    {
+        private HtmlSourceGetter _htmlSourceGetter;
+
+        private void BaseArrange()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When(VodHboBaseUrl.GetUrlWithType(MovieTypes.Action)).Respond("application/html","<html></html>");
+            var fakeClient = new HttpClient(mockHttp);
+
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            httpClientFactoryMock.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(fakeClient);
+            _htmlSourceGetter = new HtmlSourceGetter(httpClientFactoryMock.Object);
+        }
+
+        [Test]
+        public void GetHtmlFrom_ShouldReturnHtmlDocument()
+        {
+            BaseArrange();
+            var result = _htmlSourceGetter.GetHtmlFrom(VodHboBaseUrl.GetUrlWithType(MovieTypes.Action));
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.DocumentNode);
+            Assert.IsInstanceOf<HtmlDocument>(result);
+        }
+    }
+}
