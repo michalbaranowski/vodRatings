404cf7a50f01a2c748ffd032caa124c61eb16fa2
michalbaranowski <michal_baranowski@o2.pl>
2019-10-06 15:12:57 +0200

    added html source deserializer tests

---
+++ vod.Domain.Services.Tests/HtmlSourceDeserializerUT.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+using NUnit.Framework;
+using vod.Domain.Services.Boundary.Interfaces.Enums;
+using vod.Domain.Services.Tests.Resources;
+using vod.Domain.Services.Utils.HtmlSource.Deserialize;
+
+namespace vod.Domain.Services.Tests
+{
+    public class HtmlSourceDeserializerUT
+    {
+        private HtmlSourceDeserializer _deserializer;
+        private HtmlDocument _moviesHtmlDoc;
+        private HtmlDocument _moreInfoHtmlDoc;
+        private HtmlDocument _filmwebResultHtmlDoc;
+
+
+        private void Arrange()
+        {
+            _deserializer = new HtmlSourceDeserializer();
+            var moviesHtml = HtmlResources.CanalPlusThrillersResultHtml();
+            var moreInfoHtml = HtmlResources.CanalPlusConcrteMovieResultHtml();
+            var filmwebResultHtml = HtmlResources.FilmwebResultHtml();
+
+            _moviesHtmlDoc = new HtmlDocument();
+            _moviesHtmlDoc.LoadHtml(moviesHtml);
+
+            _moreInfoHtmlDoc = new HtmlDocument();
+            _moreInfoHtmlDoc.LoadHtml(moreInfoHtml);
+
+            _filmwebResultHtmlDoc = new HtmlDocument();
+            _filmwebResultHtmlDoc.LoadHtml(filmwebResultHtml);
+        }
+
+        [Test]
+        public void DeserializeMovies_ShouldDeserializeCorrectValues()
+        {
+            Arrange();
+
+            var result = _deserializer.DeserializeMovies(_moviesHtmlDoc, MovieTypes.Thriller);
+
+            Assert.True(result.Any(n=>n.Title == "Ostateczna rozgrywka"));
+            Assert.True(result.Any(n => n.MoreInfoUrl == "/Collection/Asset?codename=ostateczna-rozgrywka-19"));
+            Assert.True(result.Any(n => n.ProviderName == "CANAL+ VOD"));
+            Assert.True(result.Any(n => n.MovieType == MovieTypes.Thriller));
+        }
+
+        [Test]
+        public void DeserializeFilmwebUrl_ShouldDeserializeCorrectValues()
+        {
+            Arrange();
+
+            var result = _deserializer.DeserializeFilmwebUrl(_moreInfoHtmlDoc);
+
+            Assert.AreEqual("http://www.filmweb.pl/film/ostateczna+rozgrywka+19-2018-780203", result);
+        }
+
+        [Test]
+        public void DeserializeFilmwebResult_ShouldDeserializeCorrectValues()
+        {
+            Arrange();
+
+            var result = _deserializer.DeserializeFilmwebResult(_filmwebResultHtmlDoc, MovieTypes.Thriller);
+
+            Assert.True(result.Title == "Ostateczna rozgrywka");
+            Assert.True(result.FilmwebRating == 5.4m);
+            Assert.True(result.Year == 2018);
+        }
+    }
+}
---
+++ vod.Domain.Services.Tests/Resources/HtmlResources.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace vod.Domain.Services.Tests.Resources
+{
+    public static class HtmlResources
+    {
+        private const string BASEPATH = "../../../Resources/Htmls/";
+        public static string CanalPlusThrillersResultHtml() => ReadFromFile($"{BASEPATH}canalPlusThrillersResult.html");
+        public static string CanalPlusConcrteMovieResultHtml() => ReadFromFile($"{BASEPATH}canalPlusConcreteMovieResult.html");
+        public static string FilmwebResultHtml() => ReadFromFile($"{BASEPATH}filmwebResult.html");
+
+        private static string ReadFromFile(string path)
+        {
+            var result = string.Empty;
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+
+    }
+}
