620cf75f201e35dd7194f1b32d99a6aa6d439b5e
michalbaranowski <michal_baranowski@o2.pl>
2019-11-01 12:54:46 +0100

    added filmbox, alekino, kinotv

--- vod.Domain.Services/Utils/NcPlusUrls.cs
+++ vod.Domain.Services/Utils/NcPlusUrls.cs
@@ -6,8 +6,15 @@ namespace vod.Domain.Services.Utils
     public static class NcPlusUrls
     {
         public static readonly string NcPlusGoUrl = "http://ncplusgo.pl/";
-        public static readonly string VodPremieryBaseUrl =
-            $"{NcPlusGoUrl}Collection/Vod/Details?collectionCodename=premiery&ParentCategoryCodename=film&CollectionCategoryCodename=Vod&ViewType=collections&page=1";
+        //public static readonly string VodPremieryBaseUrl =
+        //    $"{NcPlusGoUrl}Collection/Vod/Details?collectionCodename=premiery&ParentCategoryCodename=film&CollectionCategoryCodename=Vod&ViewType=collections&page=1";
+        public static readonly string VodFilmboxBaseUrl = 
+            $"{NcPlusGoUrl}Collection/CatchUp/Details?collectionCodename=catchup-filmbox&ParentCategoryCodename=film&CollectionCategoryCodename=CatchUp&ViewType=collections&page=1";
+
+        public static readonly string VodAleKinoBaseUrl = 
+            $"{NcPlusGoUrl}Collection/CatchUp/Details?collectionCodename=catchup-ale-kino&ParentCategoryCodename=film&CollectionCategoryCodename=CatchUp&ViewType=collections&page=1";
+
+        public static readonly string VodKinoTvBaseUrl = $"{NcPlusGoUrl}Collection/CatchUp/Details?collectionCodename=filmbox-1&ParentCategoryCodename=film&CollectionCategoryCodename=CatchUp&ViewType=collections&page=1";
 
         public static readonly string VodCplusBaseUrl =
             $"{NcPlusGoUrl}Collection/CatchUp/Details?collectionCodename=catchup-canal-vod&CollectionCategoryCodename=CatchUp&ViewType=collections&ParentCategoryCodename=film&page=1";
