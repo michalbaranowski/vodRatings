463150170a0b5134f397ff1ab2001961d9481380
michalbaranowski <michal_baranowski@o2.pl>
2020-01-02 19:53:55 +0100

    fixed AddNewFlagNeeded method bug

--- vod.Core/Extension/ResultConvertExtension.cs
+++ vod.Core/Extension/ResultConvertExtension.cs
@@ -11,6 +11,8 @@ namespace vod.Core.Extension
     {
         public static IEnumerable<Result> AddNewFlagIfNeeded(this IList<Result> result)
         {
+            if (result.Any() == false) return result;
+
             var max = result.Max(n => n.StoredDate);
             var min = result.Min(n => n.StoredDate);
 
