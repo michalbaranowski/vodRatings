de17dcbcaee09fccab2344407b71c99593271d6f
michalbaranowski <michal_baranowski@o2.pl>
2020-02-08 20:30:16 +0100

    register happy path

--- vodFrontend/src/app/component/register-form-component/register-form-component.ts
+++ vodFrontend/src/app/component/register-form-component/register-form-component.ts
@@ -11,9 +11,14 @@ export class RegisterFormComponent{
 
     constructor(private authService: AuthService) {}
 
-    registerData: RegisterData;
+    registerData: RegisterData = new RegisterData();
+    passwordRepeat: string;
 
     register() {
+        if(this.passwordRepeat != this.registerData.password) {
+            return;
+        }
+
         this.authService.register(this.registerData).subscribe(registerResult => {
             if(registerResult.statusCode === 200) {
                 
